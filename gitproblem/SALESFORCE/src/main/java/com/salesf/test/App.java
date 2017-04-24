c216494faac7827349d46439af2c04deee31bd59
prasanna6358 <prasanna6358@gmail.com>
2017-04-24 17:40:19 +0530

    gitproblem

---
+++ gitproblem/SALESFORCE/src/main/java/com/salesf/test/App.java
@@ -0,0 +1,12 @@
+package com.salesf.test;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        LoginSalesForce lsf = new LoginSalesForce();
+        System.out.println(lsf.loginSalesForce());
+        //LoginSalesForce.getProperties();
+    }
+}
